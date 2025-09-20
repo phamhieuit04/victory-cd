96b746a754054cd74aae791bd433d05c5dca638f
NgocNguyen1503 <feyd153@gmail.com>
2025-09-20 11:29:37 +0700

    Process Create New Bill And Get PaymentLink

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ApiResponse;
+use App\Models\Bill;
+use App\Models\Song;
+use App\Services\PayOSService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
+
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $param = $request->all();
+        $now = Carbon::now();
+
+        try {
+            $bill = Bill::create([
+                'song_id' => $param['song_id'],
+                'order_code' => intval(substr(strval(microtime(true) * 10000), -6)),
+                'price' => 10000,
+                'status' => 1,
+                'created_at' => $now,
+                'updated_at' => $now
+            ]);
+
+            $song = Song::find($param['song_id']);
+            $item = [
+                [
+                    'name' => $song->name,
+                    'quantity' => 1,
+                    'price' => $bill->price
+                ]
+            ];
+
+            $res = PayOSService::createPaymentLink($bill, $item);
+            if (!is_null($res)) {
+                $bill->checkout_url = $res['checkoutUrl'];
+
+                return ApiResponse::success($bill);
+            } else {
+                return ApiResponse::dataNotfound();
+            }
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return ApiResponse::internalServerError();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\SongController;
 use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
@@ -10,4 +11,6 @@ Route::apiResources([
     'songs' => SongController::class
 ]);
 Route::apiResource('upload', UploadController::class)
+    ->only('store');
+Route::apiResource('payment', PaymentController::class)
     ->only('store');
