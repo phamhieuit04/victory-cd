19005dbfa18987cf502257d07d386b21520c6040
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-09-20 09:22:05 +0700

    Add app tooltip

---
+++ resources/js/components/partials/AppTooltip.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Button } from '../ui/button';
+import { Pencil, Trash2, Ellipsis } from 'lucide-vue-next';
+</script>
+
+<template>
+    <div class="flex gap-2 w-20 items-center">
+        <Button size="icon" variant="outline" class="cursor-pointer">
+            <Pencil class="size-3.5" />
+        </Button>
+        <Button size="icon" variant="outline" class="cursor-pointer">
+            <Trash2 class="size-3.5" />
+        </Button>
+    </div>
+</template>
