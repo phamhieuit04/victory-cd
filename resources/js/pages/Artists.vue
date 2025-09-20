125af110e5093fc89c30d4707e9066dfb93de105
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-09-20 09:22:31 +0700

    Update artists page

--- resources/js/pages/Artists.vue
+++ resources/js/pages/Artists.vue
@@ -1,6 +1,31 @@
 <script setup>
+import api from '@/api/axios';
+import AppTooltip from '@/components/partials/AppTooltip.vue';
 import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from '@/components/ui/table'
+import { onMounted, ref } from 'vue';
+
+var artists = ref([]);
+const total = ref(0);
+
+const getArtists = (value) => {
+    api.get('/artists', {
+        params: {
+            offset: value * 10
+        }
+    }).then((res) => {
+        if (res.status == 200) {
+            artists.value = res.data.data.artists;
+            total.value = res.data.data.total;
+        };
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+
+onMounted(() => {
+    getArtists(0);
+})
 </script>
 
 <template>
@@ -9,37 +34,39 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from '
         <Table>
             <TableHeader>
                 <TableRow>
-                    <TableHead class="w-[100px]">
-                        Invoice
-                    </TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Method</TableHead>
-                    <TableHead class="text-right">
-                        Amount
-                    </TableHead>
+                    <TableHead class="text-center">Id</TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead class="text-center">Songs</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
-                <TableRow v-for="i in 10" class="h-12 cursor-pointer">
-                    <TableCell class="font-medium">
-                        INV001
+                <TableRow v-for="artist in artists" :key="artist.id" class="h-12">
+                    <TableCell class="font-medium text-center">
+                        {{ artist.id }}
                     </TableCell>
-                    <TableCell>Paid</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell class="text-right">
-                        $250.00
+                    <TableCell>
+                        <RouterLink :to="`/artists/${artist.id}/songs`">
+                            {{ artist.name }}
+                        </RouterLink>
+                    </TableCell>
+                    <TableCell class="text-center">
+                        {{ artist.songs_count }}
+                    </TableCell>
+                    <TableCell class="w-36">
+                        <AppTooltip />
                     </TableCell>
                 </TableRow>
             </TableBody>
         </Table>
-        <Pagination>
-            <PaginationContent>
-                <PaginationPrevious />
-                <template v-for="item in 10">
-                    <PaginationItem class="cursor-pointer">{{ item }}</PaginationItem>
+        <Pagination v-slot="{ page }" :items-per-page="10" :total="total" :default-page="1">
+            <PaginationContent v-slot="{ items }">
+                <template v-for="(item, index) in items" :key="index">
+                    <PaginationItem @click="getArtists(item.value - 1)" class="cursor-pointer"
+                        v-if="item.type === 'page'" :value="item.value" :is-active="item.value === page">
+                        {{ item.value }}
+                    </PaginationItem>
                 </template>
-                <PaginationEllipsis />
-                <PaginationNext />
+                <PaginationEllipsis v-show="total > 50" />
             </PaginationContent>
         </Pagination>
     </div>
